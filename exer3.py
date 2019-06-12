637b6c50815cc250eb64b36b0d2cbb3718163dee
José Carlos <49734983+zecabanhas@users.noreply.github.com>
2019-06-11 21:10:41 -0300

    exer3

---
+++ exer3.py
@@ -0,0 +1,10 @@
+preco = 5
+tempo_excedente = 0
+tempo_minutos = int(input("Tempo utilizado: "))
+if tempo_minutos <= 10:
+ print("Valor a ser paga: ", preco)
+else:
+ tempo_minutos > 10
+ tempo_excedente = (tempo_minutos - 10)
+ preco = 5 + (tempo_excedente * 5 * 0.2)
+ print("Valor a ser pago: ", preco)
